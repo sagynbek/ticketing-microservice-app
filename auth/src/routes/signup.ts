b4f55e7b7ee0d282b88823716ad293d8f7da45c1
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-24 15:40:06 +0300

    first commit

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,28 @@
+import express, { Response, Request } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters')
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    console.log('Creating a user...');
+
+    res.send({});
+  });
+
+export { router as signupRouter };
